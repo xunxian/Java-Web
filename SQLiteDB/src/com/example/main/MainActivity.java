12b448a27442023d70c5739de33acf69b7b5c622
xunxian <382936736@qq.com>
2016-01-29 22:42:15 +0800

    select User

--- SQLiteDB/src/com/example/main/MainActivity.java
+++ SQLiteDB/src/com/example/main/MainActivity.java
@@ -5,22 +5,29 @@ import com.example.sqlitedb.R;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
-public class MainActivity extends Activity implements OnClickListener{
+
+public class MainActivity extends Activity implements OnClickListener {
 	private SQLiteDatabase db;
+	private Button insertBtn, selectBtn;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		SQLiteDB dbHelper=new SQLiteDB(MainActivity.this);
-		db=dbHelper.getWritableDatabase();
-		Button insertBtn=(Button) findViewById(R.id.db_insert);
+		SQLiteDB dbHelper = new SQLiteDB(MainActivity.this);
+		db = dbHelper.getWritableDatabase();
+		insertBtn = (Button) findViewById(R.id.db_insert);
 		insertBtn.setOnClickListener(this);
+		selectBtn = (Button) findViewById(R.id.db_select);
+		selectBtn.setOnClickListener(this);
 	}
 
 	@Override
@@ -32,13 +39,24 @@ public class MainActivity extends Activity implements OnClickListener{
 
 	@Override
 	public void onClick(View view) {
-		switch(view.getId())
-		{
+		switch (view.getId()) {
 		case R.id.db_insert:
-			ContentValues cv=new ContentValues();
+			ContentValues cv = new ContentValues();
 			cv.put("username", "Maxfang");
 			cv.put("password", "ant123456");
 			db.insert("User", null, cv);
+			break;
+		case R.id.db_select:
+			Log.d("SYS", "YOUMA");
+			Cursor cs = db.query("User", null, null, null, null, null, null);
+			Log.d("SYS", "YOUMA1");
+			if (cs.moveToFirst()) {
+				Log.d("SYS", "YOUMA2");
+				for (int i = 0; i < cs.getCount(); i++) {
+					Log.i("USER", "编号：" + cs.getString(0) + "用户名：" + cs.getString(1) + "密码：" + cs.getString(2));
+					cs.moveToNext();
+				}
+			}
 			break;
 		}
 	}
